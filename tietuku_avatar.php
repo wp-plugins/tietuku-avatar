eaa9bc1c83bc1146f29224009db31dc7e55f8b73
qakcn <qakcn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-16 10:16:42 +0000

    v0.2.0 Release
    git-svn-id: https://plugins.svn.wordpress.org/tietuku-avatar/trunk@1181764 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tietuku_avatar.php
+++ tietuku_avatar.php
@@ -2,9 +2,9 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 /**
- * Plugin Name: Tietuku Avatar
+ * Plugin Name: Tietuku Avatar for WordPress
  * Description: This WordPress plugin let you use Tietuku Avatar service to replace Gravatar
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: qakcn
  * Author URI: http://tsukkomi.org
  * License: GPLv3
@@ -29,41 +29,62 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 add_filter('get_avatar', 'tietuku_avatar', 1, 5);
 
 function tietuku_avatar( $avatar, $id_or_email, $size, $default, $alt ) {
-    $email = '';
-    if ( is_numeric($id_or_email) ) {
-        $id = (int) $id_or_email;
-        $user = get_userdata($id);
-        if ( $user )
-            $email = $user->user_email;
-    } elseif ( is_object($id_or_email) ) {
-        // No avatar for pingbacks or trackbacks
+    $user = false;
 
-        /**
-         * Filter the list of allowed comment types for retrieving avatars.
-         *
-         * @since 3.0.0
-         *
-         * @param array $types An array of content types. Default only contains 'comment'.
-         */
-        $allowed_comment_types = apply_filters( 'get_avatar_comment_types', array( 'comment' ) );
-        if ( ! empty( $id_or_email->comment_type ) && ! in_array( $id_or_email->comment_type, (array) $allowed_comment_types ) )
-            return false;
+    if ( is_numeric( $id_or_email ) ) {
+
+        $id = (int) $id_or_email;
+        $user = get_user_by( 'id' , $id );
+
+    } elseif ( is_object( $id_or_email ) ) {
 
         if ( ! empty( $id_or_email->user_id ) ) {
             $id = (int) $id_or_email->user_id;
-            $user = get_userdata($id);
-            if ( $user )
-                $email = $user->user_email;
+            $user = get_user_by( 'id' , $id );
         }
 
-        if ( ! $email && ! empty( $id_or_email->comment_author_email ) )
-            $email = $id_or_email->comment_author_email;
     } else {
-        $email = $id_or_email;
+        $user = get_user_by( 'email', $id_or_email );
     }
 
-    $avatar = 'http://avatar.tietuku.com/avatar/'.md5(strtolower(trim($email))).'?size='.$size;
-    $avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+    if ( $user && is_object( $user ) ) {
+
+        if ( $user->data->ID == '1' ) {
+            $avatar = 'http://avatar.tietuku.com/avatar/'.md5(strtolower(trim($user->email))).'?size='.$size;
+            $avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+        }
+
+    }
 
     return $avatar;
 }
+
+/* Replace Fonts and Scripts Hosted by Google with USTC mirror, for Chinese users */
+function replace_google_cdn() {
+    global $wp_scripts,$wp_styles;
+
+    foreach($wp_styles->registered as $reg) {
+        if(preg_match('/fonts\.googleapis\.com/', $reg->src)) {
+            $src = str_replace('fonts.googleapis.com','fonts.lug.ustc.edu.cn',$reg->src);
+            $handle = $reg->handle;
+            $deps = $reg->deps;
+            $ver = $reg->ver;
+            $media = $reg->args;
+            wp_deregister_style($handle);
+            wp_register_style($handle,$src,$deps,$ver,$media);
+        }
+    }
+    foreach($wp_scripts->registered as $reg) {
+        if(preg_match('/ajax\.googleapis\.com/', $reg->src)) {
+            $src = str_replace('ajax.googleapis.com','ajax.lug.ustc.edu.cn',$reg->src);
+            $handle = $reg->handle;
+            $deps = $reg->deps;
+            $ver = $reg->ver;
+            $in_footer = $reg->args;
+            wp_deregister_script($handle);
+            wp_register_script($handle,$src,$deps,$ver,$in_footer);
+        }
+    }
+}
+add_action('wp_enqueue_scripts', 'replace_google_cdn', 99999);
+add_action('admin_enqueue_scripts', 'replace_google_cdn', 99999);
